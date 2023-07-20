31d9798fe5776eafb20fd408a889d0ec514b8094
alice52 <zzhang_xz@163.com>
2023-07-20 21:45:53 +0800

    feat(syntax): add fun note

--- tutorials/kotlin-se/se-syntax/src/main/kotlin/se/syntax/keyword/TopLevelClass.kt
+++ tutorials/kotlin-se/se-syntax/src/main/kotlin/se/syntax/keyword/TopLevelClass.kt
@@ -10,7 +10,7 @@ fun main() {
 
     JvmStaticClass.aStaticFuncation()
 
-    MyObject.run()
+//    MyObject.run()
 
     ObjectExtension.extension()
 }
---
+++ tutorials/kotlin-se/se-syntax/src/main/kotlin/se/syntax/object/Sealed.kt
@@ -0,0 +1,26 @@
+package se.syntax.`object`
+
+sealed class Result
+
+class Success(val data: String) : Result()
+class Error(val message: String) : Result()
+
+fun handleResult(result: Result) {
+    when (result) {
+        is Success -> println("Success: ${result.data}")
+        is Error -> println("Error: ${result.message}")
+    }
+}
+
+fun main() {
+
+    val successResult: Result = Success("Data received!")
+    val errorResult: Result = Error("An error occurred!")
+
+    handleResult(successResult)
+    handleResult(errorResult)
+
+
+}
+
+
